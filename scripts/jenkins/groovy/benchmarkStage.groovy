e870f025546471f2f00d6b905c98e016542070f6
Michal Raška <michal.raska@gmail.com>
2018-04-04 14:51:58 +0200

    [ITA-269] Add XGB Benchmark stage.
    [ITA-269] Update benchmark image SHA
    
    [ITA-269] Mount whole 0xdiag.
    
    [ITA-273] Redirect output of docker inspect to /dev/null
    
    [ITA-269] Add expected values for xgb and vanilla xgb.
    Add vanilla xgb benchmark.
    
    [ITA-269] Update expected values

--- scripts/jenkins/groovy/benchmarkStage.groovy
+++ scripts/jenkins/groovy/benchmarkStage.groovy
@@ -6,20 +6,26 @@ def call(final pipelineContext, final stageConfig) {
   def insideDocker = load('h2o-3/scripts/jenkins/groovy/insideDocker.groovy')
 
   final String DATASETS_FILE = 'accuracy_datasets_docker.csv'
-  final GString TEST_CASES_FILE = "test_cases_${stageConfig.customData.model}.csv"
+  final GString TEST_CASES_FILE = "test_cases_${stageConfig.customData.algorithm}.csv"
   final GString ML_BENCHMARK_ROOT = "${env.WORKSPACE}/${pipelineContext.getUtils().stageNameToDirName(stageConfig.stageName)}/h2o-3/ml-benchmark"
 
-  stageConfig.datasetsPath = stageConfig.datasetsPath ?: "${ML_BENCHMARK_ROOT}/h2oR/${DATASETS_FILE}"
-  stageConfig.testCasesPath = stageConfig.testCasesPath ?: "${ML_BENCHMARK_ROOT}/h2oR/${TEST_CASES_FILE}"
+  stageConfig.datasetsPath = "${ML_BENCHMARK_ROOT}/h2oR/${DATASETS_FILE}"
+  stageConfig.testCasesPath = "${ML_BENCHMARK_ROOT}/h2oR/${TEST_CASES_FILE}"
   stageConfig.makefilePath = stageConfig.makefilePath ?: "${ML_BENCHMARK_ROOT}/jenkins/Makefile.jenkins"
 
   dir (ML_BENCHMARK_ROOT) {
-    checkout changelog: false, poll: false, scm: [$class: 'GitSCM', branches: [[name: 'master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: H2O_OPS_CREDS_ID, url: 'https://github.com/h2oai/ml-benchmark']]]
-    sh "sed 's/s3:\\/\\/h2o-benchmark/\\/datasets/g' h2oR/accuracy_datasets_h2o.csv > h2oR/accuracy_datasets_docker.csv"
+    retry(3) {
+      timeout(time: 1, unit: 'MINUTES') {
+        // FIXME set master branch
+        checkout changelog: false, poll: false, scm: [$class: 'GitSCM', branches: [[name: 'mr/ita/269-xgb-benchmarks']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: H2O_OPS_CREDS_ID, url: 'https://github.com/h2oai/ml-benchmark']]]
+      }
+    }
+    sh "sed 's/s3:\\/\\/.*\\//\\/home\\/0xdiag\\/h2o-3-benchmark\\//g;s/https:\\/\\/.*\\//\\/home\\/0xdiag\\/h2o-3-benchmark\\//g' h2oR/accuracy_datasets_h2o.csv > h2oR/accuracy_datasets_docker.csv"
+    sh 'cat h2oR/accuracy_datasets_docker.csv'
   }
 
-  def prepareBenchmarkDirStruct = load("${ML_BENCHMARK_ROOT}/jenkins/groovy/prepareBenchmarkDirStruct.groovy")
-  def benchmarkFolderConfig = prepareBenchmarkDirStruct(stageConfig.customData.model, env.GIT_SHA, env.BRANCH_NAME)
+  def prepareBenchmarkFolderConfig = pipelineContext.getPrepareBenchmarkDirStruct(this, ML_BENCHMARK_ROOT)
+  def benchmarkFolderConfig = prepareBenchmarkFolderConfig(stageConfig.customData.algorithm, env.GIT_SHA, env.BRANCH_NAME)
   GString outputPath = "${env.workspace}/${pipelineContext.getUtils().stageNameToDirName(stageConfig.stageName)}/${benchmarkFolderConfig.getOutputDir()}"
   sh "rm -rf ${outputPath} && mkdir -p ${outputPath}"
 
@@ -29,7 +35,7 @@ def call(final pipelineContext, final stageConfig) {
           "OUTPUT_PATH=${outputPath}",
           "GIT_SHA=${env.GIT_SHA}",
           "GIT_DATE=${env.GIT_DATE.replaceAll(' ', '-')}",
-          "BENCHMARK_MODEL=${stageConfig.customData.model}",
+          "BENCHMARK_ALGORITHM=${stageConfig.customData.algorithm}",
           "BUILD_ID=${env.BUILD_ID}",
   ]
 
